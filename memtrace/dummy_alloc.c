3d071892ee606e9c8a487ab3f122a6720e2b54a8
andreaslaos02 <laos.andreas238@gmail.com>
2025-10-07 19:36:46 +0300

    al

--- memtrace/dummy_alloc.c
+++ memtrace/dummy_alloc.c
@@ -5,12 +5,12 @@
    They do nothing at runtime (the Pin tool will interpose on them). */
 
 void __memtrace_alloc_site(void* ptr, size_t size, const char* type_tag, const char* func, const char* file, int line) {
-    fprintf(stderr, "[SITE ALLOC] p=%p size=%zu tag=%s @%s:%d (%s)\n",ptr, size, (type_tag ? type_tag : "?"),(file ? file : "?"), line, (func ? func : "?"));    
+    //fprintf(stderr, "[SITE ALLOC] p=%p size=%zu tag=%s @%s:%d (%s)\n",ptr, size, (type_tag ? type_tag : "?"),(file ? file : "?"), line, (func ? func : "?"));    
     (void)ptr; (void)size; (void)type_tag; (void)func; (void)file; (void)line;
 }
 
 void __memtrace_free_site(void* ptr, const char* type_tag, const char* func, const char* file, int line) {
-    fprintf(stderr, "[SITE FREE ] p=%p tag=%s @%s:%d (%s)\n",ptr, (type_tag ? type_tag : "?"),(file ? file : "?"), line, (func ? func : "?"));
+    //fprintf(stderr, "[SITE FREE ] p=%p tag=%s @%s:%d (%s)\n",ptr, (type_tag ? type_tag : "?"),(file ? file : "?"), line, (func ? func : "?"));
     (void)ptr; (void)type_tag; (void)func; (void)file; (void)line;
 }
 
